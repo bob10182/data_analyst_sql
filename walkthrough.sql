816b945c45b7c55b2881cfdede2d56dc5c10da79
Robert Ruiz <122419886+bob10182@users.noreply.github.com>
2023-02-13 16:28:08 -0600

    Add files via upload

---
+++ walkthrough.sql
@@ -0,0 +1,116 @@
+--The dataset for this exercise has been derived from the `Indeed Data Scientist/Analyst/Engineer` [dataset](https://www.kaggle.com/elroyggj/indeed-dataset-data-scientistanalystengineer) on kaggle.com. 
+
+--Before beginning to answer questions, take some time to review the data dictionary and familiarize yourself with the data that is contained in each column.
+
+--#### Provide the SQL queries and answers for the following questions/tasks using the data_analyst_jobs table you have created in PostgreSQL:
+
+--1.	How many rows are in the data_analyst_jobs table?
+
+SELECT COUNT (*)
+FROM data_analyst_jobs;
+--Answer 1793
+
+--2.	Write a query to look at just the first 10 rows. What company is associated with the job posting on the 10th row?
+SELECT data_analyst_jobs
+FROM data_analyst_jobs
+LIMIT 10;
+--Answer: Exxon Mobil
+
+--3.	How many postings are in Tennessee? How many are there in either Tennessee or Kentucky?
+SELECT COUNT (location)
+FROM data_analyst_jobs
+WHERE location IN ('TN','KY');
+
+--Answer: TN=21, TN and KY=27
+
+--4.	How many postings in Tennessee have a star rating above 4?
+SELECT COUNT (location)
+FROM data_analyst_jobs
+WHERE (location ='TN')AND (star_rating >4);
+--Answer: 3
+
+--5.	How many postings in the dataset have a review count between 500 and 1000?
+SELECT COUNT (review_count)
+FROM data_analyst_jobs
+WHERE review_count BETWEEN 500 AND 1000;
+--Answer: 151
+
+--6.	Show the average star rating for companies in each state. The output should show the state as `state` and the average rating for the state as `avg_rating. Which state shows the highest average rating?
+SELECT location AS state, AVG(star_rating) AS avg_rating
+FROM data_analyst_jobs
+WHERE star_rating IS NOT NULL
+GROUP BY (location)
+ORDER BY AVG(star_rating)DESC;
+--Answer: NE with 4.19
+
+--7.	Select unique job titles from the data_analyst_jobs table. How many are there?
+SELECT COUNT(DISTINCT title)
+FROM data_analyst_jobs;
+--Answer:881 unique jobs
+
+--8.	How many unique job titles are there for California companies?
+SELECT COUNT(DISTINCT title)
+FROM data_analyst_jobs
+WHERE location = 'CA';
+
+--Answer 230
+
+--9.	Find the name of each company and its average star rating for all companies that have more than 5000 reviews across all locations. How many companies are there with more that 5000 reviews across all locations?
+SELECT DISTINCT(company), AVG(star_rating) AS avg_rating
+FROM data_analyst_jobs
+WHERE review_count > 5000 AND company IS NOT NULL
+GROUP BY company;
+--Answer:40 businesses
+
+--10.	Add the code to order the query in #9 from highest to lowest average star rating. Which company with more than 5000 reviews across all locations in the dataset has the highest star rating? What is that rating?
+SELECT DISTINCT(company), AVG(star_rating) AS avg_rating
+FROM data_analyst_jobs
+WHERE review_count > 5000 AND company IS NOT NULL
+GROUP BY company
+ORDER BY avg_rating DESC;
+--nswer: American Express and General Motors are tied at 4.199999809
+
+--11.	Find all the job titles that contain the word ‘Analyst’. How many different job titles are there? 
+SELECT COUNT(title)
+FROM data_analyst_jobs
+WHERE LOWER (title) LIKE LOWER ('%analyst%');
+--Answer:1669, assuming you wanted a total count not DISTINCT
+
+--12.	How many different job titles do not contain either the word ‘Analyst’ or the word ‘Analytics’? What word do these positions have in common?
+SELECT title
+FROM data_analyst_jobs
+WHERE title NOT ILIKE ('%Analyst%') 
+AND title NOT ILIKE ('%Analytics%');
+	  
+	  
+--Answer: 4, Tableau is the keyword
+
+--**BONUS:**
+--You want to understand which jobs requiring SQL are hard to fill. Find the number of jobs by industry (domain) that require SQL and have been posted longer than 3 weeks. 
+-- - Disregard any postings where the domain is NULL. 
+-- - Order your results so that the domain with the greatest number of `hard to fill` jobs is at the top. 
+--  - Which three industries are in the top 4 on this list? How many jobs have been listed for more than 3 weeks for each of the top 4?
+
+SELECT domain,skill,days_since_posting
+FROM data_analyst_jobs
+WHERE (domain IS NOT NULL)
+AND (skill IN('SQL'))
+AND (days_since_posting>21)
+ORDER BY days_since_posting DESC;
+
+--Answer: Part 1: Consulting and Business Services, Computers and Electronics and Real Estate.
+--Answer: Part 2: Consutling and Business Services=5, Computers and Electronics=1, Real Estate=1, Consumer Goods and Services=2
+
+
+--BONUS BONUS--
+--1. For each company, give the company name and the difference between its star rating and the national average star rating. 
+SELECT (star_rating)-(AVG(star_rating)) AS diff_star_rating,company
+FROM data_analyst_jobs
+GROUP BY data_analyst_jobs.star_rating,data_analyst_jobs.company;
+
+
+
+--2. Using a correlated subquery: For each company, give the company name, its domain, its star rating, and its domain average star rating
+SELECT company,domain,star_rating
+FROM data_analyst_jobs;
+--3. Repeat question 2 using a CTE instead of a correlated subquery
